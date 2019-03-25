c2ac7a650ddf68e3e75c945ceb7b1a1757e5b46b
Andranique Boone <andranique.boone@gmail.com>
2019-03-25 01:22:25 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,7 +12,7 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(birthday_kids)
-  happy_birthday(birthday_kids).collect.merge do |kids_name, age|
+  happy_birthday(birthday_kids).collect.rehash do |kids_name, age|
     age > 12
     puts "You are too old for this."
   end
